6ac84cd39a900bbb308502ad91fbdc20543a2843
vishalsherekar <sherekar.vishal@gmail.com>
2024-09-27 00:13:45 +0530

    file created

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=10,num2=20,sum;
+		sum=num1+num2;
+		System.out.println("Addition Two Value = "+ sum);
+	}
+
+}
